cc2cbc907a76bf413f69e1c17ca43f27992af91e
Je-0204 <dlej0204@naver.com>
2025-11-30 12:54:04 +0900

    [Je/#44] - Delete Unused Import

--- app/src/main/java/com/example/language/ui/mypage/MypageMyvocDetailFragment.kt
+++ app/src/main/java/com/example/language/ui/mypage/MypageMyvocDetailFragment.kt
@@ -5,7 +5,6 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.graphics.Color
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -14,7 +13,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
