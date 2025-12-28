01b0a3e52aff015cc11fd286d16180506e3df524
Je-0204 <dlej0204@naver.com>
2025-12-28 22:56:28 +0900

    [Refactor] Kakao 초기화 코드 이동

--- app/src/main/java/com/example/language/app/LanguageApplication.kt
+++ app/src/main/java/com/example/language/app/LanguageApplication.kt
@@ -1,7 +1,15 @@
 package com.example.language.app
 
 import android.app.Application
+import com.example.language.BuildConfig
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class LanguageApplication: Application()
+class LanguageApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
+    }
+}
